11cfa93970361072b8d432ac4b95c309b5b7a7c6
Jia Guo <jamixcs@gmail.com>
2025-06-17 14:50:58 +0800

    add REPO_NAME of the vite.config

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -4,15 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const REPO_NAME = 'google-test' // 與 Vite 配置一致
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: `/${REPO_NAME}/`,
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
