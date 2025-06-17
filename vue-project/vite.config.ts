681a3d26ce36f55347be123673bace13e97affb3
Jia Guo <jamixcs@gmail.com>
2025-06-17 15:26:44 +0800

    add quasar

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -3,13 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 const REPO_NAME = 'google-test' // 與 Vite 配置一致
 
 // https://vite.dev/config/
 export default defineConfig({
   base: `/${REPO_NAME}/`,
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    quasar({
+      sassVariables: fileURLToPath(new URL('./src/quasar-variables.sass', import.meta.url)),
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
