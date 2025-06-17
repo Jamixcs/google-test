68d44eef9ec596dbfe578db814890c64b52e339c
Jia Guo <jamixcs@gmail.com>
2025-06-17 15:26:52 +0800

    add FormView

---
+++ vue-project/src/views/FormView.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const formRef = ref(null)
+const formData = reactive({
+  email: null,
+  password: null,
+  confirmPassword: null,
+})
+
+function onSubmit() {
+  formRef.value.validate().then((success) => {
+    if (success) {
+      // 第三方登入
+      console.log('submit the login form!')
+    } else {
+      console.log('submit failed!!!')
+    }
+  })
+}
+</script>
+
+<template>
+  <div>
+    <q-form ref="formRef" @submit.prevent="onSubmit" class="q-gutter-md">
+      <q-input
+        color="brown-5"
+        outlined
+        v-model="formData.email"
+        label="Email"
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || 'Please type your email',
+          (val) =>
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(val) ||
+            'Please type the correct email format',
+        ]"
+      >
+      </q-input>
+      <q-input
+        color="brown-5"
+        outlined
+        v-model="formData.password"
+        label="Password"
+        type="password"
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || 'Please type your password',
+          (val) => val.length >= 6 || 'The password must be longer than six characters.',
+        ]"
+      >
+      </q-input>
+      <q-input
+        color="brown-5"
+        outlined
+        v-model="formData.confirmPassword"
+        label="Confirm Password"
+        type="password"
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || 'Please type your confirm password',
+          (val) =>
+            val === formData.password ||
+            'Please enter the same confirmation password as your password.',
+        ]"
+      >
+      </q-input>
+      <q-btn color="brown-5" label="Submit" type="submit" />
+    </q-form>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
